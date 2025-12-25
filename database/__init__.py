bca7283010bea11e818a2a7fb07aa48f3bcc2d09
AlexLyuzny <lyuznyalex@gmail.com>
2025-12-25 21:37:56 +0300

    add database, env

---
+++ database/__init__.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from dotenv import load_dotenv
+import os
+
+from .database import *
+
+load_dotenv()
+
+engine = create_async_engine(f'postgresql+asyncpg://postgres:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/postgres')
+sessions = async_sessionmaker(engine)
