01118b4b0634e85d58278050e67042227babb007
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2026-01-14 19:47:01 +0300

    Update authorization.py

--- routes/authorization.py
+++ routes/authorization.py
@@ -26,24 +26,31 @@ def generate_token() -> str:
 
 
 @router.post('/register')
-async def register(login: Annotated[str, Query(max_length=30, min_length=4)],
-                   password: Annotated[str, Query(max_length=20, min_length=4)],
-                   name: Annotated[str, Query(max_length=50, min_length=1)],
-                   surname: Annotated[str, Query(max_length=50, min_length=1)],
-                   role: Annotated[str, Query(max_length=50, min_length=1)])-> JSONResponse:
+async def register(login: Annotated[str, Query()],
+                   password: Annotated[str, Query()],
+                   name: Annotated[str, Query()],
+                   surname: Annotated[str, Query()])-> JSONResponse:
     async with database.sessions.begin() as session:
         request = await session.execute(select(database.Users).where(database.Users.login == login.strip()))
         user = request.scalar_one_or_none()
         if user is not None:
             raise HTTPException(418, {'error': 'Пользователь с таким логином уже существует'})
-        if role == 'administrator':
-            raise HTTPException(400, {'error': 'Роль администратора недоступна'})
+        if not(1 <= len(name) <= 30):
+            raise HTTPException(422, {'error': 'Длина имени должна быть от 1 до 30 символов'})
+        if not(2 <= len(surname) <= 30):
+            raise HTTPException(422, {'error': 'Длина имени должна быть от 2 до 30 символов'})
+        if not(4 <= len(login) <= 20):
+            raise HTTPException(422, {'error': 'Длина логина должна быть от 4 до 20 символов'})
+        if not(6 <= len(password)):
+            raise HTTPException(422, {'error': 'Длина пароля должна быть от 6 символов'})
+        # if role == 'administrator':
+        #     raise HTTPException(400, {'error': 'Роль администратора недоступна'})
         token = generate_token()
         second_request = await session.execute(insert(database.Users).values(login=login.strip(),
                                                                              password_hash=hash_password(password.strip()),
                                                                              name=name,
                                                                              surname=surname,
-                                                                             role=role,
+                                                                             role='user',
                                                                              points=0,
                                                                              token=token))
         await session.commit()
@@ -52,19 +59,18 @@ async def register(login: Annotated[str, Query(max_length=30, min_length=4)],
 
 
 
-class LoginRestrictions(BaseModel):
-    login: constr(min_length=4, max_length=30)
-    password: constr(min_length=8, max_length=20)
-
-
 @router.post('/login')
-async def login(login: Annotated[str, Query(max_length=30, min_length=4)],
-                   password: Annotated[str, Query(max_length=20, min_length=4)]) -> JSONResponse:
+async def login(login: Annotated[str, Query()],
+                   password: Annotated[str, Query()]) -> JSONResponse:
     async with database.sessions.begin() as session:
           request = await session.execute(select(database.Users).where(database.Users.login == login.strip()))
           user = request.scalar_one_or_none()
           if user is None:
               raise HTTPException(403, {'error': 'Неверный логин или пароль'})
+          if len(login) < 4 or len(login) > 20:
+              raise HTTPException(422, {'error': 'Длина логина должна быть от 4 до 30 символов'})
+          if len(password) < 4:
+              raise HTTPException(422, {'error': 'Длина пароля должна быть больше 3 символов'})
           if hash_password(password.strip()) != user.password_hash:
               raise HTTPException(403, {'error': 'Неверный логин или пароль'})
           return utils.json_response({'token': user.token, 'id': user.id, 'name':user.name, 'surname': user.surname})
@@ -73,8 +79,8 @@ async def login(login: Annotated[str, Query(max_length=30, min_length=4)],
 
 
 
-router.post('/verify')
-async def verify_token(token: str) -> JSONResponse:
+@router.get('/verify')
+async def verify_token(token: Annotated[str, Query()]) -> JSONResponse:
     async with database.sessions.begin() as session:
         user = await utils.token_to_user(session, token)
         if user is None:
