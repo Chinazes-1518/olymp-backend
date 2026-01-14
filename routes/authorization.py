8c14c7075dab02e63d8aeed1bd0cd7f8ba0b3737
AlexLyuzny <lyuznyalex@gmail.com>
2026-01-14 19:02:14 +0300

    Update authorization

--- routes/authorization.py
+++ routes/authorization.py
@@ -1,11 +1,12 @@
 import hashlib
 from uuid import uuid4
 
-from fastapi import APIRouter, HTTPException, FastAPI
+from fastapi import APIRouter, HTTPException, FastAPI, Query
 from fastapi.responses import JSONResponse
 from sqlalchemy import select, insert, update
 from pydantic import BaseModel, constr
 from sqlalchemy.util import greenlet_spawn
+from typing import Annotated
 
 import database
 import secrets
@@ -23,30 +24,26 @@ def generate_token() -> str:
     return secrets.token_hex(24)
 
 
-class RegisterRestrictions(BaseModel):
-    login: constr(min_length=4, max_length=30)
-    password: constr(min_length=4, max_length=20)
-    name: constr(min_length=1, max_length=50)
-    surname: constr(min_length=1, max_length=50)
-    role: constr(min_length=3, max_length=20)
-
-
 
 @router.post('/register')
-async def register(data: RegisterRestrictions) -> JSONResponse:
+async def register(login: Annotated[str, Query(max_length=30, min_length=4)],
+                   password: Annotated[str, Query(max_length=20, min_length=4)],
+                   name: Annotated[str, Query(max_length=50, min_length=1)],
+                   surname: Annotated[str, Query(max_length=50, min_length=1)],
+                   role: Annotated[str, Query(max_length=50, min_length=1)])-> JSONResponse:
     async with database.sessions.begin() as session:
-        request = await session.execute(select(database.Users).where(database.Users.login == data.login.strip()))
+        request = await session.execute(select(database.Users).where(database.Users.login == login.strip()))
         user = request.scalar_one_or_none()
         if user is not None:
             raise HTTPException(418, {'error': 'Пользователь с таким логином уже существует'})
-        if data.role == 'administrator':
+        if role == 'administrator':
             raise HTTPException(400, {'error': 'Роль администратора недоступна'})
         token = generate_token()
-        second_request = await session.execute(insert(database.Users).values(login=data.login.strip(),
-                                                                             password_hash=hash_password(data.password.strip()),
-                                                                             name=data.name,
-                                                                             surname=data.surname,
-                                                                             role=data.role,
+        second_request = await session.execute(insert(database.Users).values(login=login.strip(),
+                                                                             password_hash=hash_password(password.strip()),
+                                                                             name=name,
+                                                                             surname=surname,
+                                                                             role=role,
                                                                              points=0,
                                                                              token=token))
         await session.commit()
@@ -61,26 +58,25 @@ class LoginRestrictions(BaseModel):
 
 
 @router.post('/login')
-async def login(data: LoginRestrictions) -> JSONResponse:
+async def login(login: Annotated[str, Query(max_length=30, min_length=4)],
+                   password: Annotated[str, Query(max_length=20, min_length=4)]) -> JSONResponse:
     async with database.sessions.begin() as session:
-          request = await session.execute(select(database.Users).where(database.Users.login == data.login.strip()))
+          request = await session.execute(select(database.Users).where(database.Users.login == login.strip()))
           user = request.scalar_one_or_none()
           if user is None:
               raise HTTPException(403, {'error': 'Неверный логин или пароль'})
-          if hash_password(data.password.strip()) != user.password_hash:
+          if hash_password(password.strip()) != user.password_hash:
               raise HTTPException(403, {'error': 'Неверный логин или пароль'})
           return utils.json_response({'token': user.token, 'id': user.id, 'name':user.name, 'surname': user.surname})
 
 
-class Info(BaseModel):
-    token: str
 
 
 
 router.post('/verify')
-async def verify_token(data: Info) -> JSONResponse:
+async def verify_token(token: str) -> JSONResponse:
     async with database.sessions.begin() as session:
-        user = await utils.token_to_user(session, data.token)
+        user = await utils.token_to_user(session, token)
         if user is None:
             raise HTTPException(403, {"error": "Токен не существует"})
         return utils.json_response({'token': user.token, 'id': user.id, 'name': user.name, 'surname': user.surname})
--- routes/points.py
+++ routes/points.py
@@ -24,8 +24,6 @@ async def add_points(data: Params) -> JSONResponse:
         if data.points is None or not isinstance(data, int) or data.points <= 0:
             raise HTTPException(400, {'error', 'Неверное значение очков'})
         user1 = session.query.get(data.Userid)
-        if user1 is None:
-            raise HTTPException(403, {'error': 'Пользователь не найден'})
         user1.points += data.points
         session.commit()
         return utils.json_response({'Userid': user1.Userid, 'new_points': user1.points})
